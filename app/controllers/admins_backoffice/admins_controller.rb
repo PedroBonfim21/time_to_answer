8af215a77f6153a9225b5249e4cf1dc911cb8477
Pedro Bonfim <phenrique646@gmail.com>
2023-01-11 10:47:13 -0300

    ajustando controller

--- app/controllers/admins_backoffice/admins_controller.rb
+++ app/controllers/admins_backoffice/admins_controller.rb
@@ -1,20 +1,29 @@
 class AdminsBackoffice::AdminsController < AdminsBackofficeController
+  before_action :password_check, only: [:update]
+  before_action :admin_set, only: [:edit, :update]
   def index
     @admins = Admin.all
   end
   def edit
-    @admin = Admin.find(params[:id])
   end
   def update
-    if params[:admin][:password].blank? && params[:admin][:password_confirmation].blank?
-      params[:admin].extract!(:password, :password_confirmation)
-    end
-    @admin = Admin.find(params[:id])
-    params_admin = params.require(:admin).permit(:email, :password, :password_confirmation)
     if @admin.update(params_admin)
       redirect_to admins_backoffice_admins_path, notice:"Administrador atualizado com sucesso!"
     else
       render :edit
     end
+  end
+
+  private
+  def params_admin
+    params.require(:admin).permit(:email, :password, :password_confirmation)
+  end
+  def password_check
+    if params[:admin][:password].blank? && params[:admin][:password_confirmation].blank?
+      params[:admin].extract!(:password, :password_confirmation)
+    end
+  end
+  def admin_set
+    @admin = Admin.find(params[:id])
   end
 end
