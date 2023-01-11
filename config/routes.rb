5ff5588011ae18cccccd909320170c5eb0644a7a
Pedro Bonfim <phenrique646@gmail.com>
2023-01-11 15:39:32 -0300

    adicionando e configurando subject

--- config/routes.rb
+++ config/routes.rb
@@ -1,24 +1,20 @@
 Rails.application.routes.draw do
-
-  namespace :admins_backoffice do
-    resources :admins
-  end
-
-  devise_for :users
   namespace :site do
     get 'welcome/index'
   end
-
   namespace :users_backoffice do
     get 'welcome/index'
   end
-
   namespace :admins_backoffice do
-    get 'welcome/index'
+    get'welcome/index'
+    resources :admins
+    resources :subjects
   end
 
+  devise_for :users
   devise_for :admins
-  get 'welcome/index'
+
+  get 'inicio', to: 'site/welcome#index'
 
   root to:'site/welcome#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
