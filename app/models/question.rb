7cdf59f7e25dfa7242f49f6e3c6380299af79a8d
Pedro Bonfim <phenrique646@gmail.com>
2023-01-13 00:55:14 -0300

    adc Scopes

--- app/models/question.rb
+++ app/models/question.rb
@@ -3,10 +3,14 @@ class Question < ApplicationRecord
   has_many :answers
   accepts_nested_attributes_for :answers, reject_if: :all_blank, allow_destroy: true
   paginates_per 5
-  def self.search(page, term)
-    Question.includes(:answers).where("lower(description) LIKE ?", "%#{term.downcase}%").page(page)
-  end
-  def self.last_questions(page)
-    Question.includes(:answers).order('created_at desc').page(page)
-  end
+  
+  scope :_search_, ->(page, term){
+    includes(:answers)
+    .where("lower(description) LIKE ?", "%#{term.downcase}%")
+    .page(page)
+  }
+
+  scope :last_questions, ->(page) {
+    includes(:answers).order('created_at desc').page(page)
+  }
 end
