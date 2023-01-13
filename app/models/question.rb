0d581680cad1e13b8d0479abb7ae008dbab2f231
Pedro Bonfim <phenrique646@gmail.com>
2023-01-13 11:12:26 -0300

    add contagem de perguntas por assunto

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,5 +1,5 @@
 class Question < ApplicationRecord
-  belongs_to :subject, inverse_of: :questions
+  belongs_to :subject, counter_cache: true, inverse_of: :questions
   has_many :answers
   accepts_nested_attributes_for :answers, reject_if: :all_blank, allow_destroy: true
   paginates_per 5
@@ -11,12 +11,12 @@ class Question < ApplicationRecord
   }
 
   scope :_search_, ->(page, term){
-    includes(:answers)
+    includes(:answers, :subject)
     .where("lower(description) LIKE ?", "%#{term.downcase}%")
     .page(page)
   }
 
   scope :last_questions, ->(page) {
-    includes(:answers).order('created_at desc').page(page)
+    includes(:answers, :subject).order('created_at desc').page(page)
   }
 end
