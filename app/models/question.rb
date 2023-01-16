d31073709bdfb43cbf4d766c08b04b951f821969
Pedro Bonfim <phenrique646@gmail.com>
2023-01-16 13:25:44 -0300

    adicionando estatistica para adm

--- app/models/question.rb
+++ app/models/question.rb
@@ -2,8 +2,9 @@ class Question < ApplicationRecord
   belongs_to :subject, counter_cache: true, inverse_of: :questions
   has_many :answers
   accepts_nested_attributes_for :answers, reject_if: :all_blank, allow_destroy: true
+  after_create :set_statistic
   paginates_per 5
-  
+
   scope :_search_subject_, ->(page, subject_id){
     includes(:answers, :subject)
     .where(subject_id: subject_id)
@@ -19,4 +20,9 @@ class Question < ApplicationRecord
   scope :last_questions, ->(page) {
     includes(:answers, :subject).order('created_at desc').page(page)
   }
+
+  private
+  def set_statistic
+    AdminStatistic.set_event(AdminStatistic::EVENTS[:total_questions])
+  end
 end
