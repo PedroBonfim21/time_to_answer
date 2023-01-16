0ffaef01fb8d4ed0a68190d2aceee05e7af41f27
Pedro Bonfim <phenrique646@gmail.com>
2023-01-16 11:09:09 -0300

    adicionando novos campos

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,8 +2,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  has_one :profile_user
-  accepts_nested_attributes_for :profile_user, reject_if: :all_blank
+  has_one :user_profile
+  accepts_nested_attributes_for :user_profile, reject_if: :all_blank
 
   def full_name
     [self.first_name, self.last_name].join(' ')
